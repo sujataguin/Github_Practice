38b9b30f2f732c75cd87a1d8450e982ecae583a2
sujataguin <sujataguin1@gmail.com>
2017-02-16 19:45:08 -0500

    Create TestC.java

---
+++ TestC.java
@@ -0,0 +1,13 @@
+package Package1;
+
+public class TestC {
+
+	public static void main(String[] args) {
+		
+		//This is comment in TesB class
+		
+		System.out.println("this is TestC of Github practice");
+
+	}
+
+}
