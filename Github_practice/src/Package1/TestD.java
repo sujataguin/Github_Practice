27acc9b1436d7de11f9c826f2adfde6b52b43195
sujataguin <sujataguin1@gmail.com>
2017-02-16 19:49:41 -0500

    Create TestD.java

---
+++ Github_practice/src/Package1/TestD.java
@@ -0,0 +1,13 @@
+package Package1;
+
+public class TestD {
+
+	public static void main(String[] args) {
+		
+		//This is comment in TestB class
+		
+		System.out.println("this is TestD of Github practice");
+
+	}
+
+}
